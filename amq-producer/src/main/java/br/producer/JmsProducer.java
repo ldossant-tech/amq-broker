de7358aedd2c02d8150ef06e8b278ee52c7cb810
User1 GitLab <user1@demo.redhat.com>
2026-01-06 21:53:55 -0300

    producer5

--- amq-producer/src/main/java/br/producer/JmsProducer.java
+++ amq-producer/src/main/java/br/producer/JmsProducer.java
@@ -4,8 +4,6 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.jms.ConnectionFactory;
 import jakarta.jms.JMSContext;
-import jakarta.jms.Queue;
-import jakarta.annotation.Resource;
 
 @ApplicationScoped
 public class JmsProducer {
@@ -13,12 +11,10 @@ public class JmsProducer {
     @Inject
     ConnectionFactory connectionFactory;
 
-    @Resource(lookup = "messages")
-    Queue messages;
-
     public void send(String payload) {
         try (JMSContext context = connectionFactory.createContext()) {
-            context.createProducer().send(messages, payload);
+            context.createProducer()
+                   .send(context.createQueue("insert.message"), payload);
         }
     }
 }
